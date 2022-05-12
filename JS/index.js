f3ecdad56d6ded856754737eb326c58c65ac13b5
dswolfenberger <dswolfenberger@gmail.com>
2022-05-12 16:10:12 -0700

    Code refinement - added about me section

--- JS/index.js
+++ JS/index.js
@@ -5,7 +5,11 @@ $(function(){
     });
 });
 
- /* $(document).ready(function(){ 
+/* This is alternate jQuery that doesn't require CSS - although,
+the animation runs on mouseenter/mouseleave and doesn't include .stop() parameters.
+First comment out line 2-6 in index.js and Uncomment this section line 12-23. Then comment out class .button1.on (flexbox.css line 62) */
+ 
+/* $(document).ready(function(){ 
     $(".button1").mouseenter(function(){
         $(this).animate({
             width: "130"
